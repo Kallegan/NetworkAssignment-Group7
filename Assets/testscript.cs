4f19029440e591d6fab9ae31da784c895db9648f
Kallegan <karl.berglund@futuregames.nu>
2023-01-12 14:48:07 +0100

    Update testscript.cs

--- Assets/testscript.cs
+++ Assets/testscript.cs
@@ -7,7 +7,7 @@ public class testscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //bacon bacon
     }
 
     // Update is called once per frame
