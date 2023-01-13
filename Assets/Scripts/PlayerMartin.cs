e143264ff4cf32beebc0b88fad2aedcbffc85482
Martin Ohlsson <90261294+MMAAOOHH@users.noreply.github.com>
2023-01-13 11:14:14 +0100

    Avatar

--- Assets/Scripts/PlayerMartin.cs
+++ Assets/Scripts/PlayerMartin.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Alteruna;
 using UnityEngine;
+using Avatar = Alteruna.Avatar;
 
 public class PlayerMartin : MonoBehaviour
 {
@@ -12,8 +13,13 @@ public class PlayerMartin : MonoBehaviour
     
     [SerializeField]
     private float speed = 10f;
+
+    [SerializeField] private Avatar avatar;
     private void Update()
     {
+        if (!avatar.IsMe)
+            return;
+
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * (Time.deltaTime * speed));
     }
