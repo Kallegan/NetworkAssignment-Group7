d0476823e896a1e38a2971b125c79559c98caa7c
Martin Ohlsson <90261294+MMAAOOHH@users.noreply.github.com>
2023-01-13 11:09:56 +0100

    Network movement test

--- Assets/Scripts/PlayerMartin.cs
+++ Assets/Scripts/PlayerMartin.cs
@@ -1,38 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Alteruna;
 using UnityEngine;
 
-public class PlayerMartin : Synchronizable
+public class PlayerMartin : MonoBehaviour
 {
-       // Data to be synchronized with other players in our playroom.
-       public float SynchronizedFloat = 3.0f;
-       // Used to store the previous version of our data so that we know when it has changed.
-       private float _oldSynchronizedFloat;
-       public override void DisassembleData(Reader reader, byte LOD)
-       {
-           // Set our data to the updated value we have recieved from another player.
-           SynchronizedFloat = reader.ReadFloat();
-           // Save the new data as our old data, otherwise we will immediatly think it changed again.
-           _oldSynchronizedFloat = SynchronizedFloat;
-       }
-       public override void AssembleData(Writer writer, byte LOD)
-       {
-           // Write our data so that it can be sent to the other players in our playroom.
-           writer.Write(SynchronizedFloat);
-       }
-       private void Update()
-       {
-           // If the value of our float has changed, sync it with the other players in our playroom.
-           if (SynchronizedFloat != _oldSynchronizedFloat)
-           {
-               // Store the updated value
-               _oldSynchronizedFloat = SynchronizedFloat;
-               // Tell Alteruna that we want to commit our data.
-               Commit();
-           }
-           // Update the Synchronizable
-           base.SyncUpdate();
-       }
+
+    [SerializeField]
+    private CharacterController controller;
     
+    [SerializeField]
+    private float speed = 10f;
+    private void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * (Time.deltaTime * speed));
+    }
 }
