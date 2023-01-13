ba3c7dd9141c6ad94e2acc42d0577ea15f242409
Martin Ohlsson <90261294+MMAAOOHH@users.noreply.github.com>
2023-01-13 13:50:47 +0100

    Movement test, Alteruna

--- Assets/Scripts/PlayerMartin.cs
+++ Assets/Scripts/PlayerMartin.cs
@@ -2,32 +2,54 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Alteruna;
+using Alteruna.Trinity;
 using UnityEngine;
 using Avatar = Alteruna.Avatar;
 using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
-public class PlayerMartin : MonoBehaviour
+public class PlayerMartin : Synchronizable
 {
-
-    [SerializeField]
-    private CharacterController controller;
-
-    [SerializeField] private GameObject projectile;
-    
-    [SerializeField]
-    private float speed = 10f;
-
     [SerializeField] private Avatar avatar;
-
+    
     private Camera cam;
     
+    // Networking
+    public Vector3 SynchronizedPosition;
+    private Vector3 _oldSynchronizedPosition;
+    
+    // Movement
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed = 10f;
+    
+    // Attack    
+    [SerializeField] private GameObject projectile;
+    
+    // Health
+    private int health = 10;
+
     private void Awake()
     {
         cam = Camera.main;
     }
 
+    void Start()
+    {
+        Multiplayer.RegisterRemoteProcedure("MyProcedureName", MyProcedureFunction);
+    }
+    
+    // Local function, on every client
+    void MyProcedureFunction(ushort fromUser, ProcedureParameters parameters, uint callId, ITransportStreamReader processor)
+    {
+        Debug.Log("Hello");
+    }
+    
+    void CallMyProcedure()
+    {
+        Multiplayer.InvokeRemoteProcedure("MyProcedureName", UserId.All);
+    }
+    
     private void Update()
     {
         if (!avatar.IsMe)
@@ -50,11 +72,49 @@ public class PlayerMartin : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
             Shoot();
+        
+        // Networking
+        SynchronizedPosition = transform.position;
+        // If the value of our float has changed, sync it with the other players in our playroom.
+        if (SynchronizedPosition != _oldSynchronizedPosition)
+        {
+            // Store the updated value
+            _oldSynchronizedPosition = SynchronizedPosition;
+
+            // Tell Alteruna that we want to commit our data.
+            Commit();
+        }
+        // Update the Synchronizable
+        base.SyncUpdate();
     }
-
-
+    
     private void Shoot()
     {
         Instantiate(projectile, transform.position + transform.forward, transform.rotation);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        UpdateHealthBar();
+    }
+    private void UpdateHealthBar()
+    {
+        // Update ui
+    }
+
+    public override void AssembleData(Writer writer, byte LOD)
+    {
+        // Write our data so that it can be sent to the other players in our playroom.
+        writer.Write(SynchronizedPosition);
+    }
+
+    public override void DisassembleData(Reader reader, byte LOD)
+    {
+        // Set our data to the updated value we have recieved from another player.
+        SynchronizedPosition = reader.ReadVector3();
+
+        // Save the new data as our old data, otherwise we will immediatly think it changed again.
+        _oldSynchronizedPosition = SynchronizedPosition;
     }
 }
---
+++ Assets/Scripts/ProjectileTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Alteruna;
+using Alteruna.Trinity;
+using UnityEngine;
+
+public class ProjectileTest : Synchronizable
+{
+    public Vector3 SynchronizedPosition;
+    private Vector3 _oldSynchronizedPosition;
+    
+    private void Awake()
+    {
+        // säga hej vi har skapats
+    }
+    
+    void Update()
+    {
+        transform.position += transform.forward * (Time.deltaTime * 20);
+        SynchronizedPosition = transform.position;
+        
+        // If the value of our float has changed, sync it with the other players in our playroom.
+        if (SynchronizedPosition != _oldSynchronizedPosition)
+        {
+            // Store the updated value
+            _oldSynchronizedPosition = SynchronizedPosition;
+
+            // Tell Alteruna that we want to commit our data.
+            Commit();
+        }
+
+        // Update the Synchronizable
+        base.SyncUpdate();
+    }
+
+    public override void DisassembleData(Reader reader, byte LOD)
+    {
+        // Set our data to the updated value we have recieved from another player.
+        SynchronizedPosition = reader.ReadVector3();
+
+        // Save the new data as our old data, otherwise we will immediatly think it changed again.
+        _oldSynchronizedPosition = SynchronizedPosition;
+    }
+
+    public override void AssembleData(Writer writer, byte LOD)
+    {
+        // Write our data so that it can be sent to the other players in our playroom.
+        writer.Write(SynchronizedPosition);
+    }
+    
+    void OnBecameInvisible() 
+    {
+        Destroy(gameObject);
+    }
+
+    void DebugPrint()
+    {
+        
+    }
+}
