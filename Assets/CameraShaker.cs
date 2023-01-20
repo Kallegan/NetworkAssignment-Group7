c7ec7d2e2553090c0a80f289538d1cf151fe0375
Martin Ohlsson <90261294+MMAAOOHH@users.noreply.github.com>
2023-01-20 15:21:18 +0100

    CameraShake

---
+++ Assets/CameraShaker.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Camera))]
+public class CameraShaker : MonoBehaviour
+{
+    private Transform _t;
+    private Vector3 _startPosition;
+    private bool _shaking = false;
+    private void Awake()
+    {
+        _t = GetComponent<Transform>();
+    }
+    
+    void OnEnable()
+    {
+        _startPosition = _t.localPosition;
+        _shaking = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Shake(0.3f,0.24f);
+        }
+    }
+
+    public void Shake(float shakeTime, float strength)
+    {
+        if (_shaking) return;
+        StartCoroutine(ShakeRoutine(shakeTime, strength));
+    }
+    
+    private IEnumerator ShakeRoutine(float shakeTime, float strength)
+    {
+        _shaking = true;
+        float elapsed = 0;
+        while (elapsed <= shakeTime)
+        {
+            elapsed += Time.deltaTime;
+            _t.transform.localPosition = _startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+        _t.position = _startPosition;
+        _shaking = false;
+    }
+}
