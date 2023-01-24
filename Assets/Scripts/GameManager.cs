21f3b75dd0099847d4ade49a64f377434f7a35b7
Notuses <tobias.sunna@gmail.com>
2023-01-24 16:22:01 +0100

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -170,6 +170,9 @@ public class GameManager : Synchronizable
         {
             if (Multiplayer.Instance.Me.Index != 0)
             {
+#if UNITY_EDITOR
+                PrintDebug("GameManager - ME IS INDEX: ", Multiplayer.Instance.Me.Index);
+#endif
                 ProcedureParameters parameters = new ProcedureParameters();
                 Multiplayer.InvokeRemoteProcedure("RequestGameSettingsProcedure", Multiplayer.GetUser(0),
                     parameters);
